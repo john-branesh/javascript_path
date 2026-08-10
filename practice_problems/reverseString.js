44e50bd5e1f948d54ecac3fff7cd74b45635aef8
John Branesh <johnbraneshherin@gmail.com>
2026-08-10 22:19:26 +0530

    reverse string using 2 poointer method

--- practice_problems/reverseString.js
+++ practice_problems/reverseString.js
@@ -2,16 +2,27 @@ function reverseString(str) {
     return splti('').reverse('').join('');
 }
 
+// two pointer method
 function reverseStringManual(str){
-    let arr = split('');
+    // Turn the text into a list of single letters.
+    let arr = str.split('');
 
-    let left =0; let right = arr.length-1;
+    // Start one marker at the first letter.
+    let left = 0;
+    // Start the other marker at the last letter.
+    let right = arr.length - 1;
 
-    while(left<right){
-        [arr[left], arr[right]] = [arr[right], arr[left]] 
+    // Keep going until the two markers meet in the middle.
+    while (left < right) {
+        // Swap the letters at the left and right positions.
+        [arr[left], arr[right]] = [arr[right], arr[left]];
+
+        // Move the left marker one step toward the middle.
         left++;
+        // Move the right marker one step toward the middle.
         right--;
-       }
+    }
 
+    // Turn the list of letters back into one text string.
     return arr.join('');
 }
