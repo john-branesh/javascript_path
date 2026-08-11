3ecaed17bb230773653f52ab7c0d28466a74f0a8
John Branesh <johnbraneshherin@gmail.com>
2026-08-11 17:28:23 +0530

    plaindrom learnt and modified with inline comments

--- practice_problems/palindrome.js
+++ practice_problems/palindrome.js
@@ -1,18 +1,21 @@
 //we can use two pointer here also
 
-function palindrome(str){
-    let arr = str.split('');
+function ispalindrome(str){
+    //change the str into uniform text format
+    const text = str.toLowerCase().replace(/[^a-z0-9]/g, '');
     let left =0;
-    let right = arr.length - 1;
+    //since text is a string we can use str.length to get the length of the string
+    let right = text.length - 1;
 
     while (left<right){
-        if (!arr[left] === arr[right]){
+        if (text[left] !== text[right]){
             return false;
-            left++;
-            right--;
-        } else {
-            return true;
         }
+        //keep this out of if statement because we want to move the pointers regardless of whether the characters match or not
+        left++;
+        right--;
     }
-    return arr.join('');
+    return true;
 }
+
+console.log(ispalindrome("A man, a plan, a caal: Panama"));
